fbd14ab90791c0b4d771682c93d5098281280eca
Stevenson Nelli <stevenson.nelli@accenture.com>
2023-05-15 16:41:40 +0530

    dashboard overview changes

--- src/components/DashboardOverview/DashboardOverview.tsx
+++ src/components/DashboardOverview/DashboardOverview.tsx
@@ -14,7 +14,7 @@ interface WatermarkConfig {
 
 const DashboardOverview = () => {
   const [config, setConfig] = React.useState<WatermarkConfig>({
-    content: "Ant Design",
+    content: "Qatar Development Bank",
     color: "rgba(0, 0, 0, 0.15)",
     fontSize: 16,
     zIndex: 11,
@@ -42,30 +42,26 @@ const DashboardOverview = () => {
     <Watermark {...watermarkProps}>
       <Typography>
         <Paragraph>
-          The light-speed iteration of the digital world makes products more
-          complex. However, human consciousness and attention resources are
-          limited. Facing this design contradiction, the pursuit of natural
-          interaction will be the consistent direction of Ant Design.
+          QDB Banking services are designed to cater to the financial needs of
+          individuals, businesses, and governments. Banks provide a variety of
+          services to their clients, including but not limited to:
         </Paragraph>
         <Paragraph>
-          Natural user cognition: According to cognitive psychology, about 80%
-          of external information is obtained through visual channels. The most
-          important visual elements in the interface design, including layout,
-          colors, illustrations, icons, etc., should fully absorb the laws of
-          nature, thereby reducing the user&apos;s cognitive cost and bringing
-          authentic and smooth feelings. In some scenarios, opportunely adding
-          other sensory channels such as hearing, touch can create a richer and
-          more natural product experience.
+          Deposits: QDB accept deposits from individuals and businesses and pay
+          interest on the deposited amount. Deposits can be in the form of
+          savings accounts, current accounts, fixed deposits, and recurring
+          deposits.
         </Paragraph>
         <Paragraph>
-          Natural user behavior: In the interaction with the system, the
-          designer should fully understand the relationship between users,
-          system roles, and task objectives, and also contextually organize
-          system functions and services. At the same time, a series of methods
-          such as behavior analysis, artificial intelligence and sensors could
-          be applied to assist users to make effective decisions and reduce
-          extra operations of users, to save users&apos; mental and physical
-          resources and make human-computer interaction more natural.
+          Loans: QDB provide loans to individuals and businesses for various
+          purposes, such as education, housing, and business expansion. The
+          interest rates on loans vary depending on the type of loan and the
+          borrower's creditworthiness.
+        </Paragraph>
+        <Paragraph>
+          Credit cards: Banks issue credit cards to their clients that allow
+          them to make purchases on credit. Credit cards come with various
+          benefits, such as rewards and cashback offers.
         </Paragraph>
       </Typography>
       <img
@@ -78,6 +74,31 @@ const DashboardOverview = () => {
         src="https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*zx7LTI_ECSAAAAAAAAAAAABkARQnAQ"
         alt="示例图片"
       />
+      <Typography>
+        <Paragraph>
+          Investment services: Banks offer investment services such as mutual
+          funds, stocks, bonds, and other investment options. These services
+          help clients grow their wealth and achieve their financial goals.
+        </Paragraph>
+        <Paragraph>
+          Foreign exchange services: Banks provide foreign exchange services to
+          their clients, which include currency exchange, traveler's checks, and
+          international money transfers.
+        </Paragraph>
+        <Paragraph>
+          Insurance services: Banks also offer insurance products such as life
+          insurance, health insurance, and general insurance to help their
+          clients protect themselves and their assets.
+        </Paragraph>
+        <Paragraph>
+          Online banking: With the advent of technology, most banks now offer
+          online banking services, allowing clients to access their accounts and
+          perform transactions from anywhere at any time. Overall, banks play a
+          critical role in the financial well-being of their clients by
+          providing them with various financial services and products to meet
+          their needs.
+        </Paragraph>
+      </Typography>
     </Watermark>
   );
 };
